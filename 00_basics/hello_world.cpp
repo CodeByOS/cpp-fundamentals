e62da64fd7d81b742de35fad6c2cd0da77263b7e
Oussama <saidi.oussama2004@gmail.com>
2025-05-27 23:54:13 +0100

    initial commit..

---
+++ 00_basics/hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  //* Preprocessor directive to include input-output stream library
+
+//* The main function - entry point of every C++ program
+int main() {
+    // Output "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
