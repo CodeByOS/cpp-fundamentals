1815e6c5b69ba50f221eecd360aa15633960c2cf
Oussama <saidi.oussama2004@gmail.com>
2025-06-04 23:41:17 +0100

    Switch Case Statement Explain...

---
+++ 02_control_flow/switch_case.cpp
@@ -0,0 +1,48 @@
+/*
+    ! 🔄 Switch-Case in C++
+
+    The `switch` statement allows you to run different blocks of code based on the value of a variable.
+
+    * Used instead of multiple `if-else` statements
+    * Compares a value against multiple `case` labels
+    * Executes matching case block
+    * `break` prevents fall-through
+    * `default` runs if no match is found
+
+    Best for handling multiple known values of a single variable.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int day = 3; // Example variable to switch on
+
+    switch (day) {
+        case 1:
+            cout << "Monday" << endl;
+            break; //! Prevents fall-through to the next case
+        case 2:
+            cout << "Tuesday" << endl;
+            break;
+        case 3:
+            cout << "Wednesday" << endl;
+            break;
+        case 4:
+            cout << "Thursday" << endl;
+            break;
+        case 5:
+            cout << "Friday" << endl;
+            break;
+        case 6:
+            cout << "Saturday" << endl;
+            break;
+        case 7:
+            cout << "Sunday" << endl;
+            break;
+        default:
+            cout << "Invalid day" << endl; // Default case
+    }
+
+    return 0;
+}
